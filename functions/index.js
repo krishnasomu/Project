ab242da5fa3e3c928c07f2795a86594b053b2cd4
krishna somu <37823838+krishnasomu@users.noreply.github.com>
2018-09-26 17:55:30 +0530

    cors

--- functions/index.js
+++ functions/index.js
@@ -26,6 +26,59 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   }
 });
 
+exports.sendMessageToBot = functions.https.onRequest((request, response) => {
+
+  const cors = require('cors');
+  const express = require('express');
+  let app = express();
+  app.use(cors());
+  app.options('*', cors());
+
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  /*
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  response.header("Access-Control-Allow-Headers", "content-type, accept, my-header");
+  response.header("Access-Control-Max-Age", 1000);
+  response.header("Access-Control-Allow-Credentials", true);
+  response.header("content-length", 1000);
+  response.header("Content-Type", "text/html");
+  response.flushHeaders();
+  */
+
+  /*
+  response.writeHead(
+    201,
+    {
+        "Access-Control-Allow-Origin": "https://www.somu.co.in",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "content-type, accept, my-header",
+        "Access-Control-Max-Age": 1000, // Seconds.
+        "Access-Control-Allow-Credentials": true,
+        "content-length": 1000,
+        "Content-Type":"text/html"
+    }
+  );
+  */
+
+  console.log("header set");
+
+  /*
+  response.writeHead(
+    200,
+    {
+    }
+  );
+  */
+  //response.status(200);
+  response.write("response from sendMessageToBot");
+  response.end();
+
+  return //response.end();
+});
+
 exports.sendEmail = functions.https.onRequest((request, response) => {
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
@@ -525,6 +578,7 @@ function processV2Request (request, response) {
         var strIntent = request.body.queryResult.intent["displayName"];
         var strSessionID = request.body["session"];
         var strPhotos = '';
+        var image = new Image('');
   
         console.log("sessionid: " + strSessionID);
 
@@ -555,6 +609,7 @@ function processV2Request (request, response) {
                   var strPlaces = snapshot.child(child.key + "/places").val();
                   var strOcassion = snapshot.child(child.key + "/ocassion").val();
                   if(strDate.indexOf(photo_year)>-1 && strPersons.indexOf(family_member)>-1 && strPlaces.indexOf(photo_location)>-1){
+                    image.setImage('https://www.somu.co.in/images/photos/' + child.key + '.jpg');
                     strPhotos = strPhotos + child.key + ',';
                   }
                 });
